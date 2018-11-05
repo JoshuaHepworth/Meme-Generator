936dea592490f7566de21bd07ff261fc7321af63
Josh Hepworth <jrhepworth12@gmail.com>
2018-11-05 15:47:37 -0600

    added auth controller and session

--- server.js
+++ server.js
@@ -2,17 +2,17 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const sesssion = require('express-session')
 const User = require('./models/user')
 const Meme = require('./models/meme')
 const Image = require('./models/image')
+const session = require('express-session')
 
 require('./db/db')
-// app.use(session({
-// 	secret: 'memes dopeness',
-// 	resave: false,
-// 	saveUninitialized: false
-// }))
+app.use(session({
+	secret: 'memes dopeness',
+	resave: false,
+	saveUninitialized: false
+}))
 
 //MIDDLEWARE
 app.use(bodyParser.urlencoded({extended: false}))
@@ -29,9 +29,10 @@ app.use(cors(corsOptions));
 const userController = require('./controllers/userController')
 const memeController = require('./controllers/memeController')
 const imageController = require('./controllers/imageController')
+const authController = require('./controllers/authController')
 
 app.use('/api/v1/images', imageController);
-
+app.use('/auth', authController);
 app.use('/api/v1/memes', memeController);
 
 app.listen(process.env.PORT || 5000, () => {
