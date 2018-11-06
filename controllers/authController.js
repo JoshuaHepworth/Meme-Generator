8293b53caa97a926c7d0a3a1d54fa1c5b620dd84
Andy Prudhomme <aprudhomme9@gmail.com>
2018-11-06 14:12:29 -0600

    messing with userController

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,46 +12,28 @@ router.post('/', async (req, res) => {
 		const user = await User.create(req.body)
 		console.log(user, 'here is the user')
 		req.session.logged = true;
-<<<<<<< HEAD
-		req.session.username = req.body.username;
-		console.log(session.body.username)
-=======
+
+		// req.session.username = req.body.username;
+		// console.log(session.body.username)
+
 		console.log(req.session.logged, '<--logged?');
 		req.session.username = user.username;
 		req.session.ID = user._id;
 		console.log(req.session.username, '<--username?');
 		console.log(req.session.ID, '<---ID');
-		user.save();
->>>>>>> 584e19f8fa66ce9fcd6c3abb5607212e1bbad02c
+		await user.save();
+		console.log(user, 'stupid fucking user');
+		console.log(req.session, 'goddamn session');
 		res.json({
 			status: 200,
-			data: 'login successful'
+			data: user
 		});
 	} catch(err){
 		console.log(err)
 	}
-});
-
-<<<<<<< HEAD
-router.get('/logout', (req, res) => {
-	req.session.destroy((err) => {
-		if(err) res.send(err);
-		}
-	})
-=======
-router.get('/', async (req, res) => {
-	try {
-		console.log(req.session.ID, 'can get ID');
-		const foundUser = await User.findById(req.session.ID);
-		console.log(foundUser, 'found him');
-		res.json({
-			status: 200,
-			data: foundUser
-		})
-	} catch (err) {
-		res.send(err)
-	}
 })
->>>>>>> 584e19f8fa66ce9fcd6c3abb5607212e1bbad02c
+
+
+
 
 module.exports = router;
--- controllers/userController.js
+++ controllers/userController.js
@@ -5,27 +5,12 @@ const router = express.Router()
 const User = require('../models/user')
 const session = require('express-session')
 
-<<<<<<< HEAD
-router.get('/', async (req, res) => {
-    try {
-		const foundUser = await User.findOne({username: req.session.username});
-		// console.log(foundUser, 'this is the user');
-=======
 // route to user profile page
-<<<<<<< HEAD
 router.get('/', async (req, res) => {
 	try {
-		console.log(req.session.ID, 'can get ID');
-		const foundUser = await User.findById(req.session.ID);
+		console.log(req.session.username, 'can get username');
+		const foundUser = await User.findOne({username: 'test test'});
 		console.log(foundUser, 'found him');
-=======
-router.get('/', async (req, res, next) => {
-    try {
-    	console.log(req.session.username);
-		const foundUser = await User.findById(req.session.ID)
-		console.log(foundUser, '<--found the user');
->>>>>>> 584e19f8fa66ce9fcd6c3abb5607212e1bbad02c
->>>>>>> a84a9260468b4d790de99a553abdc2463bb0907a
 		res.json({
 			status: 200,
 			data: foundUser
@@ -34,10 +19,6 @@ router.get('/', async (req, res, next) => {
 		res.send(err)
 	}
 })
-<<<<<<< HEAD
-module.exports = router
-=======
 
 module.exports = router
 
->>>>>>> 584e19f8fa66ce9fcd6c3abb5607212e1bbad02c
