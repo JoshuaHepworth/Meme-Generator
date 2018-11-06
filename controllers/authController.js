822642bcd1de00b4f6dd1d383fc02df97ec56299
Josh Hepworth <jrhepworth12@gmail.com>
2018-11-06 14:49:43 -0600

    made logout route

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,27 +27,21 @@ router.post('/', async (req, res) => {
 		const user = await User.create(req.body)
 		console.log(user, 'here is the user')
 		req.session.logged = true;
-<<<<<<< HEAD
 		// req.session.username = req.body.username;
 		console.log(session.body.username)
-=======
+
 
 		// req.session.username = req.body.username;
 		// console.log(session.body.username)
-
->>>>>>> 8293b53caa97a926c7d0a3a1d54fa1c5b620dd84
 		console.log(req.session.logged, '<--logged?');
 		req.session.username = user.username;
 		req.session.ID = user._id;
 		console.log(req.session.username, '<--username?');
 		console.log(req.session.ID, '<---ID');
-<<<<<<< HEAD
 		user.save();
-=======
 		await user.save();
 		console.log(user, 'stupid fucking user');
 		console.log(req.session, 'goddamn session');
->>>>>>> 8293b53caa97a926c7d0a3a1d54fa1c5b620dd84
 		res.json({
 			status: 200,
 			data: user
@@ -55,21 +49,18 @@ router.post('/', async (req, res) => {
 	} catch(err){
 		console.log(err)
 	}
-<<<<<<< HEAD
+
 });
 
-// <<<<<<< HEAD
-// router.get('/logout', (req, res) => {
-// 	req.session.destroy((err) => {
-// 		if(err) res.send(err);
-// 		}
-// 	})
-// =======
-=======
+router.get('/', (req, res) => {
+	req.session.destroy((err) => {
+		if(err){
+			console.log(err)
+		}
+	})
 })
 
 
 
->>>>>>> 8293b53caa97a926c7d0a3a1d54fa1c5b620dd84
 
 module.exports = router;
