e41e4f601556e6c4086d2a4e2167c9975c8e86a8
Andy Prudhomme <aprudhomme9@gmail.com>
2018-11-20 16:45:34 -0600

    more fixing for heroku

--- server.js
+++ server.js
@@ -29,6 +29,11 @@ const corsOptions = {
 	optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //CONTROLLERS
 const userController  = require('./controllers/userController')
